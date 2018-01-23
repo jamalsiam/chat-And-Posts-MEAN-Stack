344dc665cced207c6851a0694a89b9d0aaef9fc7
jamalsiam <jamalsiam1995@gmail.com>
2018-01-23 20:41:40 +0200

    post controller

--- server/handlers/postController.js
+++ server/handlers/postController.js
@@ -1,11 +1,32 @@
+let model=require('../models');
 
 module.exports = {
-
-    getData: function(req, res) {
-
+    sharePost: (req, res) => {
+        let {_id, postContent}=req.body;
+        model.Post.create({userId:_id,postContent},(post)=>{
+            res.json({status:"success"})
+        })
     },
-    postData:function(req, res) {
-      
+    deletePost: (req, res) => {
+        let {userId, postId}=req.body;
+        model.Post.findByIdAndRemove({userId,_id:postId}, (err, result) => {
+            res.json({status:"success"})
+        
+        })
+    },
+    getProfilePost: (req, res) => {
+        let {userId}=req.body;
+        model.Post.find({userId})
+         .then((data) => {
+            res.json({status:"success", data})
+         })
+    },
+    getOnePost: (req, res) => {
+        let {postId}=req.body;
+        model.Post.findOne({_id:postId})
+         .then((data) => {
+            res.json({status:"success", data})
+         })
     }
 };
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 let app = require('./setup');
 let handlers=app.handlers;
 
+//user API
 app.post('/api/user/signup', handlers.handelUser.signUp);
 app.post('/api/user/signin', handlers.handelUser.signIn);
 app.post('/api/user/getuserinfo',handlers.handelUser.getUserInfo);
 app.post('/api/user/postdata',handlers.handelUser.postData);
+
+//post API
+app.post('/api/post/sharepost',handlers.handelPost.sharePost);
+app.post('/api/post/deletepost',handlers.handelPost.deletePost);
+app.post('/api/post/getprofilepost',handlers.handelPost.getProfilePost);
+app.post('/api/post/getonepost',handlers.handelPost.getOnePost);
