39d641eaac33f0a2478871f119c8785d4d287e0b
jamalsiam <jamalsiam1995@gmail.com>
2018-01-23 20:28:09 +0200

    user controller

--- server/handlers/userController.js
+++ server/handlers/userController.js
@@ -1,10 +1,61 @@
-
+let model=require('../models');
 module.exports = {
+    signUp: (req, res) => {
+        let {email, username, password}=req.body;
 
-    getData: function(req, res) {
+        if( password.length >= 8 ){
+        model.User.findOne({email: email})
+            .exec( (err, user) => {
+                if (user) {
+                    res.json({status:'User already exist!'});
+                } else {
+                    // make a new user if not one
+                    return model.User.create({
+                        username: username,
+                        email: email,
+                        password: password
+                    },(err, newUser) =>{
+                        if(err){
+                            res.json(err);
+                        } else {
+                            res.json({status:'signup',id:newUser._id});
+                        }
+                    });
+                }
+            });
+        }else
+            {
+            res.json({status:'the password is short'})
+        }
+    },
+    signIn: (req, res) => {
+        let {email, password}=req.body;
 
+        model.User.findOne({email: email})
+            .then((users) => {
+                if (!users) {
+                    res.json({status:"user not found"})
+                } else {
+                    users.comparePasswords(password)
+                        .then( (isMatch) => {
+                            if (isMatch) {
+                                res.json({status:"signin",id:users._id})
+                            } else {
+                                res.json({status:"password not matched"})
+                            }
+                        });
+                }
+            });
+    },
+    getUserInfo:(req, res) => {
+        let {userId}=req.body;
+        model.Post.findOne({_id:userId})
+         .then((data) => {
+            res.json({status:"success",data})
+         })
     },
     postData:function(req, res) {
+    
         console.log(req.body)
         res.json('ss')
     }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 let app = require('./setup');
 let handlers=app.handlers;
 
-app.post('/api/postdata',handlers.handelUser.postData);
+app.post('/api/user/signup', handlers.handelUser.signUp);
+app.post('/api/user/signin', handlers.handelUser.signIn);
+app.post('/api/user/getuserinfo',handlers.handelUser.getUserInfo);
+app.post('/api/user/postdata',handlers.handelUser.postData);
