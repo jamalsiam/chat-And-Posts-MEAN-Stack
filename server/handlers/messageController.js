f08171b1dfbbab6ed41d85314da76b8273a1ec4e
jamalsiam <jamalsiam1995@gmail.com>
2018-01-23 22:00:51 +0200

    send message -fix error

--- server/handlers/messageController.js
+++ server/handlers/messageController.js
@@ -2,8 +2,8 @@ let Message=require('../models').Message;
 module.exports = {
 
     sendMessage:(req, res) => {
-        let {senderId,senderId,messageContent}=req.body;
-        Message.create({senderId,senderId,messageContent},(result)=>{
+        let {senderId,receiverId,messageContent}=req.body;
+        Message.create({senderId,receiverId,messageContent},(result)=>{
             res.json({status:"success",data:"sent"});
         })
     }
