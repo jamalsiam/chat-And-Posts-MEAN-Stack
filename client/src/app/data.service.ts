c166ea12580c4b269500d781d097cac0c99c4ac7
jamalsiam <jamalsiam1995@gmail.com>
2018-03-01 11:15:44 +0200

    change image profile

--- client/src/app/data.service.ts
+++ client/src/app/data.service.ts
@@ -1,112 +1,120 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import { Http , Headers } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { LocalStorageService } from 'angular-2-local-storage';
 
 @Injectable()
 
 export class DataService {
-  
-   user:any;
-   followingPost:any;
+
+  user: any;
+  followingPost: any;
   constructor(private http: Http, public storage: LocalStorageService) {
-    this.user={id:null, post:[] ,userInfo:{},follow:{}}
-    if(this.storage.get('chatUserId') !== null || this.storage.get('chatUserId') !== undefined){
-      this.user['id']=this.storage.get('chatUserId');
-     
-      this.getProfileInfo({userId:''+this.storage.get('chatUserId')}).subscribe(res =>{
-       
-        let post=res.post;
-        let userInfo=res.userInfo;
-        
-        for(let i=0;i<post.length;i++){
-          post[i]['imageUser']=userInfo.image;
-          post[i]['username']=userInfo.username;
-          post[i]['userId']=userInfo._id;
+    this.user = { id: null, post: [], userInfo: {}, follow: {} }
+    if (this.storage.get('chatUserId') !== null || this.storage.get('chatUserId') !== undefined) {
+      this.user['id'] = this.storage.get('chatUserId');
+
+      this.getProfileInfo({ userId: '' + this.storage.get('chatUserId') }).subscribe(res => {
+
+        let post = res.post;
+        let userInfo = res.userInfo;
+
+        for (let i = 0; i < post.length; i++) {
+          post[i]['imageUser'] = userInfo.image;
+          post[i]['username'] = userInfo.username;
+          post[i]['userId'] = userInfo._id;
         }
-        
-        this.user=res;
-        this.user['post']=post;
-      })
+
+        this.user = res;
+        this.user['post'] = post;
+      });
     }
-   
+
   }
-  
+
   createAuthorizationHeader(headers: Headers) {
     headers.append('Authorization', 'Basic ' +
       btoa('a20e6aca-ee83-44bc-8033-b41f3078c2b6:c199f9c8-0548-4be79655-7ef7d7bf9d20'));
   }
 
 
-  signUp(record)  { 
+  signUp(record) {
     let headers: Headers;
     headers = new Headers();
     this.createAuthorizationHeader(headers);
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/user/signup', record, {headers: headers}).map(res => res.json());
+    return this.http.post('/api/user/signup', record, { headers: headers }).map(res => res.json());
   }
   signUpAsGuest() {
-     return this.http.get('/api/user/signupasguest').map(res => res.json());
-   }
-   signIn(record)  { 
+    return this.http.get('/api/user/signupasguest').map(res => res.json());
+  }
+  signIn(record) {
     let headers: Headers;
     headers = new Headers();
     this.createAuthorizationHeader(headers);
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/user/signin', record, {headers: headers}).map(res => res.json());
+    return this.http.post('/api/user/signin', record, { headers: headers }).map(res => res.json());
   }
-  getProfileInfo(record)  { 
+  getProfileInfo(record) {
     let headers: Headers;
     headers = new Headers();
     this.createAuthorizationHeader(headers);
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/user/getuserinfo', record, {headers: headers}).map(res => res.json());
+    return this.http.post('/api/user/getuserinfo', record, { headers: headers }).map(res => res.json());
   }
-  signOut(){
+  signOut() {
     this.storage.remove('chatUserId');
-    this.user={};
-    this.followingPost=[];
+    this.user = {};
+    this.followingPost = [];
   }
-  sharePost (record)  { 
+  sharePost(record) {
     let headers: Headers;
     headers = new Headers();
     this.createAuthorizationHeader(headers);
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/post/sharepost', record, {headers: headers}).map(res => res.json());
+    return this.http.post('/api/post/sharepost', record, { headers: headers }).map(res => res.json());
   }
-  getFollowingPosts (record)  { 
+  getFollowingPosts(record) {
     let headers: Headers;
     headers = new Headers();
     this.createAuthorizationHeader(headers);
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/post/getFollowingPosts', record, {headers: headers}).map(res => res.json());
+    return this.http.post('/api/post/getFollowingPosts', record, { headers: headers }).map(res => res.json());
   }
-  deletePost(record)  { 
+  deletePost(record) {
     let headers: Headers;
     headers = new Headers();
     this.createAuthorizationHeader(headers);
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/post/deletepost', record, {headers: headers}).map(res => res.json());
+    return this.http.post('/api/post/deletepost', record, { headers: headers }).map(res => res.json());
   }
-  randomSuggest()  { 
+  randomSuggest() {
     return this.http.get('/api/post/randomSuggest').map(res => res.json());
-  
+
   }
-  checkIfFollow(record){ 
+  checkIfFollow(record) {
     let headers: Headers;
     headers = new Headers();
     this.createAuthorizationHeader(headers);
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/follow/checkiffollow', record, {headers: headers}).map(res => res.json());
+    return this.http.post('/api/follow/checkiffollow', record, { headers: headers }).map(res => res.json());
   }
-  
-  follow(record){ 
+
+  follow(record) {
     let headers: Headers;
     headers = new Headers();
     this.createAuthorizationHeader(headers);
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/follow/addorremovefollow', record, {headers: headers}).map(res => res.json());
+    return this.http.post('/api/follow/addorremovefollow', record, { headers: headers }).map(res => res.json());
   }
-  
+
+  changeProfileImage(record) {
+    let headers: Headers;
+    headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('/api/user/changeprofileimage', record, { headers: headers }).map(res => res.json());
+  }
+
 }
--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -9,29 +9,50 @@ import { DataService } from '../data.service';
 export class ProfileComponent implements OnInit {
   tapRouteVal: String = 'Posts';
   photoPost: string;
-  constructor(public service: DataService) {  }
+  constructor(public service: DataService) { }
 
   routeTabs(a: string) {
     this.tapRouteVal = a;
   }
-  handleFileSelect(evt) {
+  /*Change image profile*/
+  handleFileSelectImage(evt) {
     const files = evt.target.files;
     const file = files[0];
 
     if (files && file) {
       const reader = new FileReader();
-      reader.onload = this._handleReaderLoaded.bind(this);
+      reader.onload = this._handleReaderLoadedImage.bind(this);
       reader.readAsBinaryString(file);
     }
   }
-
-  _handleReaderLoaded(readerEvt) {
+  _handleReaderLoadedImage(readerEvt) {
     const binaryString = readerEvt.target.result;
     this.service.user.userInfo['image'] = btoa(binaryString);
+  this.service.changeProfileImage({}).subscribe(res => {
+    this.service.user.userInfo['image'] = btoa(binaryString);
+  });
+  }
+ /*END Change image profile*/
+
+  /*Change cover profile*/
+  handleFileSelectCover(evt) {
+    const files = evt.target.files;
+    const file = files[0];
+    if (files && file) {
+      const reader = new FileReader();
+      reader.onload = this._handleReaderLoadedCover.bind(this);
+      reader.readAsBinaryString(file);
+    }
+  }
+  _handleReaderLoadedCover(readerEvt) {
+    const binaryString = readerEvt.target.result;
+    this.service.user.userInfo['cover'] = btoa(binaryString);
 
   }
+/*END Change cover profile*/
+
   ngOnInit() {
 
-   }
+  }
 
 }
